ee8b000b9814a5830745a4b08088cd1ffb445e1e
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-27 02:30:46 +0300

    updated the header/footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,191 +1,161 @@
-    <!-- ========== FOOTER ========== -->
-    <footer class="bg-light">
-        <div class="container pb-1 pb-lg-7">
-            <div class="row content-space-t-2">
-                <div class="col-lg-3 mb-7 mb-lg-0">
-                    <!-- Logo -->
-                    <div class="mb-5">
-                        <a class="navbar-brand" href="index.html" aria-label="Space">
-                            <img class="navbar-brand-logo" src="assets/svg/logos/logo.svg" alt="Image Description">
-                        </a>
-                    </div>
-                    <!-- End Logo -->
-
-                    <!-- List -->
-                    <ul class="list-unstyled list-py-1">
-                        <li><a class="link-sm link-secondary" href="#"><i class="bi-geo-alt-fill me-1"></i> 153 Williamson Plaza, Maggieberg</a></li>
-                        <li><a class="link-sm link-secondary" href="tel:1-062-109-9222"><i class="bi-telephone-inbound-fill me-1"></i> +1 (062) 109-9222</a></li>
-                    </ul>
-                    <!-- End List -->
-
+<!-- ========== FOOTER ========== -->
+<footer class="bg-light">
+    <div class="container pb-1 pb-lg-7">
+        <div class="row content-space-t-2">
+            <!-- Logo and Contact Info -->
+            <div class="col-lg-3 mb-7 mb-lg-0">
+                <!-- Logo -->
+                <div class="mb-5">
+                    <a class="navbar-brand" href="home.php" aria-label="White Rose Cleaners">
+                        <img class="navbar-brand-logo" src="assets/svg/logos/logo.svg" alt="Logo">
+                    </a>
                 </div>
-                <!-- End Col -->
+                <!-- End Logo -->
 
-                <div class="col-sm mb-7 mb-sm-0">
-                    <h5 class="mb-3">Company</h5>
-
-                    <!-- List -->
-                    <ul class="list-unstyled list-py-1 mb-0">
-                        <li><a class="link-sm link-secondary" href="#">About</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Careers <span class="badge bg-warning text-dark rounded-pill ms-1">We're hiring</span></a></li>
-                        <li><a class="link-sm link-secondary" href="#">Blog</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Customers <i class="bi-box-arrow-up-right small ms-1"></i></a></li>
-                        <li><a class="link-sm link-secondary" href="#">Hire us</a></li>
-                    </ul>
-                    <!-- End List -->
-                </div>
-                <!-- End Col -->
-
-                <div class="col-sm mb-7 mb-sm-0">
-                    <h5 class="mb-3">Features</h5>
-
-                    <!-- List -->
-                    <ul class="list-unstyled list-py-1 mb-0">
-                        <li><a class="link-sm link-secondary" href="#">Press <i class="bi-box-arrow-up-right small ms-1"></i></a></li>
-                        <li><a class="link-sm link-secondary" href="#">Release Notes</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Integrations</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Pricing</a></li>
-                    </ul>
-                    <!-- End List -->
-                </div>
-                <!-- End Col -->
-
-                <div class="col-sm mb-7 mb-sm-0">
-                    <h5 class="mb-3">Documentation</h5>
-
-                    <!-- List -->
-                    <ul class="list-unstyled list-py-1 mb-0">
-                        <li><a class="link-sm link-secondary" href="#">Support</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Docs</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Status</a></li>
-                        <li><a class="link-sm link-secondary" href="#">API Reference</a></li>
-                        <li><a class="link-sm link-secondary" href="#">Tech Requirements</a></li>
-                    </ul>
-                    <!-- End List -->
-                </div>
-                <!-- End Col -->
-
-                <div class="col-sm">
-                    <h5 class="mb-3">Resources</h5>
-
-                    <!-- List -->
-                    <ul class="list-unstyled list-py-1 mb-5">
-                        <li><a class="link-sm link-secondary" href="#"><i class="bi-question-circle-fill me-1"></i> Help</a></li>
-                        <li><a class="link-sm link-secondary" href="#"><i class="bi-person-circle me-1"></i> Your Account</a></li>
-                    </ul>
-                    <!-- End List -->
-                </div>
-                <!-- End Col -->
+                <!-- Contact Info -->
+                <ul class="list-unstyled list-py-1">
+                    <li><a class="link-sm link-secondary" href="#"><i class="bi-geo-alt-fill me-1"></i> 1234 Cleaning Ave, CleanCity</a></li>
+                    <li><a class="link-sm link-secondary" href="tel:+1234567890"><i class="bi-telephone-inbound-fill me-1"></i> +1 (234) 567-890</a></li>
+                    <li><a class="link-sm link-secondary" href="mailto:info@whiterosecleaners.com"><i class="bi-envelope-fill me-1"></i> info@whiterosecleaners.com</a></li>
+                </ul>
+                <!-- End Contact Info -->
             </div>
-            <!-- End Row -->
+            <!-- End Logo and Contact Info -->
 
-            <div class="border-top my-7"></div>
-
-            <div class="row mb-7">
-                <div class="col-sm mb-3 mb-sm-0">
-                    <!-- Socials -->
-                    <ul class="list-inline list-separator mb-0">
-                        <li class="list-inline-item">
-                            <a class="text-body" href="#">Privacy &amp; Policy</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="text-body" href="#">Terms</a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a class="text-body" href="#">Site Map</a>
-                        </li>
-                    </ul>
-                    <!-- End Socials -->
-                </div>
-
-                <div class="col-sm-auto">
-                    <!-- Socials -->
-                    <ul class="list-inline mb-0">
-                        <li class="list-inline-item">
-                            <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
-                                <i class="bi-facebook"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
-                                <i class="bi-google"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
-                                <i class="bi-twitter"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
-                                <i class="bi-github"></i>
-                            </a>
-                        </li>
-
-                        <li class="list-inline-item">
-                            <!-- Button Group -->
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-soft-secondary btn-xs dropdown-toggle" id="footerSelectLanguage" data-bs-toggle="dropdown" aria-expanded="false" data-bs-dropdown-animation>
-                                    <span class="d-flex align-items-center">
-                                        <img class="avatar avatar-xss avatar-circle me-2" src="assets/vendor/flag-icon-css/flags/1x1/us.svg" alt="Image description" width="16" />
-                                        <span>English (US)</span>
-                                    </span>
-                                </button>
-
-                                <div class="dropdown-menu" aria-labelledby="footerSelectLanguage">
-                                    <a class="dropdown-item d-flex align-items-center active" href="#">
-                                        <img class="avatar avatar-xss avatar-circle me-2" src="assets/vendor/flag-icon-css/flags/1x1/us.svg" alt="Image description" width="16" />
-                                        <span>English (US)</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="#">
-                                        <img class="avatar avatar-xss avatar-circle me-2" src="assets/vendor/flag-icon-css/flags/1x1/de.svg" alt="Image description" width="16" />
-                                        <span>Deutsch</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="#">
-                                        <img class="avatar avatar-xss avatar-circle me-2" src="assets/vendor/flag-icon-css/flags/1x1/es.svg" alt="Image description" width="16" />
-                                        <span>Español</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="#">
-                                        <img class="avatar avatar-xss avatar-circle me-2" src="assets/vendor/flag-icon-css/flags/1x1/cn.svg" alt="Image description" width="16" />
-                                        <span>中文 (繁體)</span>
-                                    </a>
-                                </div>
-                            </div>
-                            <!-- End Button Group -->
-                        </li>
-                    </ul>
-                    <!-- End Socials -->
-                </div>
+            <!-- Useful Links -->
+            <div class="col-sm mb-7 mb-sm-0">
+                <h5 class="mb-3">Useful Links</h5>
+                <!-- List -->
+                <ul class="list-unstyled list-py-1">
+                    <li><a class="link-sm link-secondary" href="home.php">Home</a></li>
+                    <li><a class="link-sm link-secondary" href="about.php">About Us</a></li>
+                    <li><a class="link-sm link-secondary" href="blogs.php">Blog</a></li>
+                    <li><a class="link-sm link-secondary" href="faqs.php">FAQs</a></li>
+                    <li><a class="link-sm link-secondary" href="contact.php">Contact Us</a></li>
+                </ul>
+                <!-- End List -->
             </div>
+            <!-- End Useful Links -->
 
-            <!-- Copyright -->
-            <div class="w-md-85 text-lg-center mx-lg-auto">
-                <p class="text-muted small">&copy; Front. 2021 Htmlstream. All rights reserved.</p>
-                <p class="text-muted small">When you visit or interact with our sites, services or tools, we or our authorised service providers may use cookies for storing information to help provide you with a better, faster and safer experience and for marketing purposes.</p>
+            <!-- Commercial Cleaning -->
+            <div class="col-sm mb-7 mb-sm-0">
+                <h5 class="mb-3">Commercial Cleaning</h5>
+                <!-- List -->
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=office-cleaning">Office Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=industrial-cleaning">Industrial Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=retail-cleaning">Retail Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=event-cleaning">Event Cleaning</a></li>
+                </ul>
+                <!-- End List -->
             </div>
-            <!-- End Copyright -->
+            <!-- End Commercial Cleaning -->
+
+            <!-- Residential Cleaning -->
+            <div class="col-sm mb-7 mb-sm-0">
+                <h5 class="mb-3">Residential Cleaning</h5>
+                <!-- List -->
+                <ul class="list-unstyled list-py-1 mb-0">
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=regular-cleaning">Regular Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=deep-cleaning">Deep Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=move-in-move-out-cleaning">Move In/Move Out Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=post-construction-cleaning">Post-Construction Cleaning</a></li>
+                </ul>
+                <!-- End List -->
+            </div>
+            <!-- End Residential Cleaning -->
+
+            <!-- Specialized Cleaning -->
+            <div class="col-sm">
+                <h5 class="mb-3">Specialized Cleaning</h5>
+                <!-- List -->
+                <ul class="list-unstyled list-py-1 mb-5">
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=carpet-cleaning">Carpet Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=window-cleaning">Window Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=upholstery-cleaning">Upholstery Cleaning</a></li>
+                    <li><a class="link-sm link-secondary" href="single-service.php?service=eco-friendly-cleaning">Eco-Friendly Cleaning</a></li>
+                </ul>
+                <!-- End List -->
+            </div>
+            <!-- End Specialized Cleaning -->
         </div>
-    </footer>
-    <!-- ========== END FOOTER ========== -->
+        <!-- End Row -->
 
+        <div class="border-top my-7"></div>
 
-    <!-- Go To Top-->
-    <a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden;" data-hs-go-to-options='{
-       "offsetTop": 700,
-       "position": {
-         "init": {
-           "right": "2rem"
-         },
-         "show": {
-           "bottom": "2rem"
-         },
-         "hide": {
-           "bottom": "-2rem"
-         }
-       }
-     }'>
-        <i class="bi-chevron-up"></i>
-    </a>
+        <div class="row mb-7">
+            <div class="col-sm mb-3 mb-sm-0">
+                <!-- Socials -->
+                <ul class="list-inline list-separator mb-0">
+                    <li class="list-inline-item">
+                        <a class="text-body" href="#">Privacy &amp; Policy</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-body" href="#">Terms</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-body" href="#">Site Map</a>
+                    </li>
+                </ul>
+                <!-- End Socials -->
+            </div>
+
+            <div class="col-sm-auto">
+                <!-- Socials -->
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item">
+                        <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
+                            <i class="bi-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
+                            <i class="bi-google"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
+                            <i class="bi-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn btn-soft-secondary btn-xs btn-icon" href="#">
+                            <i class="bi-github"></i>
+                        </a>
+                    </li>
+                </ul>
+                <!-- End Socials -->
+            </div>
+        </div>
+
+        <!-- Copyright -->
+        <div class="w-md-85 text-lg-center mx-lg-auto">
+            <p class="text-muted small">&copy; <span id="currentYear"></span> White Rose Cleaners. All rights reserved.</p>
+            <p class="text-muted small">Built by <a href="https://ganiamtech.com" class="text-primary">Ganiam Tech</a></p>
+        </div>
+        <!-- End Copyright -->
+    </div>
+</footer>
+<!-- ========== END FOOTER ========== -->
+
+<!-- Go To Top -->
+<a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden;" data-hs-go-to-options='{
+   "offsetTop": 700,
+   "position": {
+     "init": {
+       "right": "2rem"
+     },
+     "show": {
+       "bottom": "2rem"
+     },
+     "hide": {
+       "bottom": "-2rem"
+     }
+   }
+ }'>
+    <i class="bi-chevron-up"></i>
+</a>
+
+<script>
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+</script>

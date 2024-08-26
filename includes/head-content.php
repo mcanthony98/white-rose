67dd12f3b3f81f9f61bcdf576955b9af0fc864f2
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-08-27 01:39:56 +0300

    added global elements & starter code

---
+++ includes/head-content.php
@@ -0,0 +1,17 @@
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&amp;display=swap" rel="stylesheet">
+
+    <!-- CSS Implementing Plugins -->
+    <link rel="stylesheet" href="assets/css/vendor.min.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/vendor/aos/dist/aos.css">
+
+    <!-- CSS Front Template -->
+    <link rel="stylesheet" href="assets/css/theme.minc619.css?v=1.0">
---
+++ includes/scripts.php
@@ -0,0 +1,100 @@
+    <!-- JS Implementing Plugins -->
+    <script src="assets/js/vendor.min.js"></script>
+    <script src="assets/vendor/aos/dist/aos.js"></script>
+
+    <!-- JS Front -->
+    <script src="assets/js/theme.min.js"></script>
+
+    <!-- JS Plugins Init. -->
+    <script>
+        (function() {
+            // INITIALIZATION OF HEADER
+            // =======================================================
+            new HSHeader('#header').init()
+
+
+            // INITIALIZATION OF MEGA MENU
+            // =======================================================
+            new HSMegaMenu('.js-mega-menu', {
+                desktop: {
+                    position: 'left'
+                }
+            })
+
+
+            // INITIALIZATION OF SHOW ANIMATIONS
+            // =======================================================
+            new HSShowAnimation('.js-animation-link')
+
+
+            // INITIALIZATION OF BOOTSTRAP VALIDATION
+            // =======================================================
+            HSBsValidation.init('.js-validate', {
+                onSubmit: data => {
+                    data.event.preventDefault()
+                    alert('Submited')
+                }
+            })
+
+
+            // INITIALIZATION OF BOOTSTRAP DROPDOWN
+            // =======================================================
+            HSBsDropdown.init()
+
+
+            // INITIALIZATION OF GO TO
+            // =======================================================
+            new HSGoTo('.js-go-to')
+
+
+            // INITIALIZATION OF AOS
+            // =======================================================
+            AOS.init({
+                duration: 650,
+                once: true
+            });
+
+
+            // INITIALIZATION OF TEXT ANIMATION (TYPING)
+            // =======================================================
+            HSCore.components.HSTyped.init('.js-typedjs')
+
+
+            // INITIALIZATION OF SWIPER
+            // =======================================================
+            var sliderThumbs = new Swiper('.js-swiper-thumbs', {
+                watchSlidesVisibility: true,
+                watchSlidesProgress: true,
+                history: false,
+                breakpoints: {
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    },
+                },
+                on: {
+                    'afterInit': function(swiper) {
+                        swiper.el.querySelectorAll('.js-swiper-pagination-progress-body-helper')
+                            .forEach($progress => $progress.style.transitionDuration = `${swiper.params.autoplay.delay}ms`)
+                    }
+                }
+            });
+
+            var sliderMain = new Swiper('.js-swiper-main', {
+                effect: 'fade',
+                autoplay: true,
+                loop: true,
+                thumbs: {
+                    swiper: sliderThumbs
+                }
+            })
+        })()
+    </script>
